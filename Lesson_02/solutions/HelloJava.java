4048376f18656ec6343c61b45dd3e9b204208143
Jessica Skeete <jessicaskeete@new-host.home>
2013-09-16 22:38:28 -0400

    Adding solutions to lesson 2

---
+++ Lesson_02/solutions/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+
+	//This is my first Java Program.
+	public static void main (String[] args){
+		System.out.println("Welcome to MSTU 4031 @ Teachers College, Columbia University");
+	}
+}
---
+++ Lesson_02/solutions/Variables.java
@@ -0,0 +1,19 @@
+/* File: 
+ * By: Jessica T-Skeete
+ * Email: jessicat@tc.columbia.edu
+ * Date: 9/17/2013
+ * Comments: variable with values
+*/
+
+public class Variables{
+	public static void main(String[] args){
+		String firstName = "John"; //declare and assign variable ‘firstName’
+		int age = 25;
+		float phoneNumber = 2125551234;
+
+
+	    System.out.println(firstName); //output variable firstName
+	    System.out.println( ); //output variable age
+	    System.out.println( ); //output variable phoneNumber
+	}
+}
